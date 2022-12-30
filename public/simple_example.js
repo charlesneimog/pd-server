939221a0bf89360788354c6ab8df96356e30b9c5
Charles K. Neimog <charlesneimog@outlook.com>
2022-12-30 13:47:46 -0300

    add js examples

--- public/simple_example.js
+++ public/simple_example.js
@@ -19,71 +19,66 @@ function updateprogress(duration) {
 
 // =======================================
 function sendNumber() {
-    // post aleatoric number to Pure Data
     var xhr = new XMLHttpRequest();
-    // get the local host address
     var host = window.location.hostname;
-    // get the port number
     var port = window.location.port;
-    // set the url
-    var url = 'http://' + host + ':' + port + '/send2pd';
-    // open the connection
+    var protocol = window.location.protocol;
+    var url = protocol + '//' + host + ':' + port + '/send2pd'; // WARNING: This is an standard, all the requests must be sent to this url
     xhr.open('POST', url, true);
-    // set the content type
     xhr.setRequestHeader('Content-Type', 'application/json');
-    // send the request
     var random_number = Math.round(Math.random() * 100);
-
-    // make list of random numbers
-    var list = [];
-    for (var i = 0; i < 10; i++) {
-        list.push(Math.round(Math.random() * 100));
-    }
     xhr.send(JSON.stringify({ 'number': random_number}));
 }
 
 // =======================================
 function sendMessage() {
-    // post aleatoric number to Pure Data
     var xhr = new XMLHttpRequest();
-    // get the local host address
     var host = window.location.hostname;
-    // get the port number
     var port = window.location.port;
-    // set the url
-    var url = 'http://' + host + ':' + port + '/send2pd';
-    // open the connection
+    var protocol = window.location.protocol;
+    var url = protocol + '//' + host + ':' + port + '/send2pd'; // WARNING: This is an standard, all the requests must be sent to this url
     xhr.open('POST', url, true);
-    // set the content type
     xhr.setRequestHeader('Content-Type', 'application/json');
-    // send the request
     var message = document.getElementById('message').value;
-    // make list of random numbers
     xhr.send(JSON.stringify({ 'symbol': message}));
 }
 
 // =======================================
 function sendList() {
-    // post aleatoric number to Pure Data
     var xhr = new XMLHttpRequest();
-    // get the local host address
     var host = window.location.hostname;
-    // get the port number
     var port = window.location.port;
-    // set the url
-    var url = 'http://' + host + ':' + port + '/send2pd';
-    // open the connection
+    var protocol = window.location.protocol;
+    var url = protocol + '//' + host + ':' + port + '/send2pd'; // WARNING: This is an standard, all the requests must be sent to this url
     xhr.open('POST', url, true);
-    // set the content type
     xhr.setRequestHeader('Content-Type', 'application/json');
-    // send the request
     var list = [];
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 100; i++) {
         list.push(Math.round(Math.random() * 100));
     }
     xhr.send(JSON.stringify({ 'list': list}));
 }
 
+// =======================================
+// TODO: implement websocket
+
+// function startWebsocket() {
+//     // get the local host address
+//     var host = window.location.hostname;
+//     // get the port number
+//     var port = window.location.port;
+//     // set the url
+//     var url = 'ws://' + host + ':' + port + '/ws';
+//     // open the connection
+//     var ws = new WebSocket(url);
+//     // set the onmessage function
+//     ws.onmessage = function (event) {
+//         var data = JSON.parse(event.data);
+//         var score = data.score;
+//         var scoretime = document.getElementById('ScoreTime');
+//         scoretime.innerHTML = score;
+//     };
+// }
 
 // =======================================
 function updateimage() {
